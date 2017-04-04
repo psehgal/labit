33b5c23436ae77aebf5943b3dc1128b2b2878ba2
Parth Sehgal <parth.sehgal@gatech.edu>
2017-04-03 22:41:17 -0400

    updated the way hl7 messages are created

--- messagegenerator/hl7tools.py
+++ messagegenerator/hl7tools.py
@@ -1,6 +1,7 @@
 from hl7apy.core import Message, Segment
 from models import Practitioner, Patient, LabTest
 import random
+import pdb
 
 # These are from Robert's email to us with HL7 messages
 SENDING_APPLICATION = "13^1.2.840.114350.1.13.244.2.7.2.695071^ISO"
@@ -27,43 +28,63 @@ def generate_hl7_message(practitioner, patient, tests, critical):
     hl7message.msh.msh_3 = SENDING_APPLICATION
     hl7message.msh.msh_4 = SENDING_FACILITY
     hl7message.msh.msh_5 = RECEIVING_APPLICATION
+    hl7message.msh.msh_9 = "ORU^R01^ORU_R01"
+    hl7message.msh.msh_10 = str(random.randint(1,100))
+    hl7message.msh.msh_11 = str(random.randint(1, 100))
+    hl7message.msh.msh_12 = str(random.randint(1, 100))
 
     pid = Segment("PID")
-    pid.pid_1 = patient
+    hl7message.ORU_R01_PATIENT_RESULT.ORU_R01_PATIENT.PID.pid_1 = patient
+    hl7message.ORU_R01_PATIENT_RESULT.ORU_R01_PATIENT.PID.pid_3 = patient
     # TODO: last, first
-    pid.pid_5 = patient_object.name.replace(' ', '^')
-    pid.pid_7 = patient_object.birthday
-    pid.pid_8 = patient_object.sex[0]
+    hl7message.ORU_R01_PATIENT_RESULT.ORU_R01_PATIENT.PID.pid_5 = patient_object.name.replace(' ', '^')
+    hl7message.ORU_R01_PATIENT_RESULT.ORU_R01_PATIENT.PID.pid_7 = patient_object.birthday
+    hl7message.ORU_R01_PATIENT_RESULT.ORU_R01_PATIENT.PID.pid_8 = patient_object.sex[0]
     #10 - Race
     #11 - Address
-    #Should those be queried from fhir?
+    #Should those ^ be queried from fhir?
     #More PID fields here, but I think the above are necessary... https://corepointhealth.com/resource-center/hl7-resources/hl7-pid-segment
-    hl7message.add(pid)
 
-    pv1 = Segment("PV1")
-    #PV1 - Patient Visit Information Segement (https://corepointhealth.com/resource-center/hl7-resources/hl7-pv1-patient-visit-information-segment)
-    #PV1_3 is patient location - ask Cotterman if this has room number and how to parse it.
-    pv1.pv1_2 = "E"
-    pv1.pv1_3 = "ED"
-    pv1.pv1_7 = practitioner
-    hl7message.add(pv1)
+    # pv1 = Segment("PV1")
+    # #PV1 - Patient Visit Information Segement (https://corepointhealth.com/resource-center/hl7-resources/hl7-pv1-patient-visit-information-segment)
+    # #PV1_3 is patient location - ask Cotterman if this has room number and how to parse it.
+    # pv1.pv1_2 = "E"
+    # pv1.pv1_3 = "ED"
+    # pv1.pv1_7 = practitioner
+    # hl7message.add(pv1)
 
-    for test in tests:
+    for i, test in enumerate(tests):
         test_object = LabTest.objects.get(test_name=test)
-        obr = Segment('OBR')
-        obr.obr_4 = test_object.loinc_id
-        hl7message.add(obr)
-        obx = Segment('OBX')
-        obx.obx_3 = test.replace(" ", "^")
-        obx.obx_4 = test_object.loinc_id
-        value, test_range = patient_object.get_test_value(test_object, critical)
-        obx.obx_5 = str(value)
-        obx.obx_6 = test_range.test_units
-        obx.obx_7 = str(test_range.test_reference_range_lower) + "-" + str(test_range.test_reference_range_upper)
-        if critical:
-            obx.obx_8 = "AA"
+        if i == 0:
+            hl7message.ORU_R01_PATIENT_RESULT.ORU_R01_ORDER_OBSERVATION.OBR.obr_4 = test_object.loinc_id
+            hl7message.ORU_R01_PATIENT_RESULT.ORU_R01_ORDER_OBSERVATION.ORU_R01_OBSERVATION.OBX.obx_3 = test.replace(" ", "^")
+            hl7message.ORU_R01_PATIENT_RESULT.ORU_R01_ORDER_OBSERVATION.ORU_R01_OBSERVATION.OBX.obx_4 = test_object.loinc_id
+            value, test_range = patient_object.get_test_value(test_object, critical)
+            hl7message.ORU_R01_PATIENT_RESULT.ORU_R01_ORDER_OBSERVATION.ORU_R01_OBSERVATION.OBX.obx_5 = str(value)
+            hl7message.ORU_R01_PATIENT_RESULT.ORU_R01_ORDER_OBSERVATION.ORU_R01_OBSERVATION.OBX.obx_6 = test_range.test_units
+            hl7message.ORU_R01_PATIENT_RESULT.ORU_R01_ORDER_OBSERVATION.ORU_R01_OBSERVATION.OBX.obx_7 = str(test_range.test_reference_range_lower) + "-" + str(test_range.test_reference_range_upper)
+            hl7message.ORU_R01_PATIENT_RESULT[i].ORU_R01_ORDER_OBSERVATION.ORU_R01_OBSERVATION.OBX.obx_11 = "F"
+            if critical:
+                hl7message.ORU_R01_PATIENT_RESULT.ORU_R01_ORDER_OBSERVATION.ORU_R01_OBSERVATION.OBX.obx_8 = "AA"
+            else:
+                hl7message.ORU_R01_PATIENT_RESULT.ORU_R01_ORDER_OBSERVATION.ORU_R01_OBSERVATION.OBX.obx_6 = "N"
         else:
-            obx.obx_8 = "N"
-        hl7message.add(obx)
+            hl7message.add_group("ORU_R01_PATIENT_RESULT")
+            hl7message.ORU_R01_PATIENT_RESULT[i].ORU_R01_ORDER_OBSERVATION.OBR.obr_4 = test_object.loinc_id
+            hl7message.ORU_R01_PATIENT_RESULT[i].ORU_R01_ORDER_OBSERVATION.ORU_R01_OBSERVATION.OBX.obx_3 = test.replace(
+                " ", "^")
+            hl7message.ORU_R01_PATIENT_RESULT[i].ORU_R01_ORDER_OBSERVATION.ORU_R01_OBSERVATION.OBX.obx_4 = test_object.loinc_id
+            value, test_range = patient_object.get_test_value(test_object, critical)
+            hl7message.ORU_R01_PATIENT_RESULT[i].ORU_R01_ORDER_OBSERVATION.ORU_R01_OBSERVATION.OBX.obx_5 = str(value)
+            hl7message.ORU_R01_PATIENT_RESULT[i].ORU_R01_ORDER_OBSERVATION.ORU_R01_OBSERVATION.OBX.obx_6 = test_range.test_units
+            hl7message.ORU_R01_PATIENT_RESULT[i].ORU_R01_ORDER_OBSERVATION.ORU_R01_OBSERVATION.OBX.obx_7 = str(test_range.test_reference_range_lower) + "-" + str(test_range.test_reference_range_upper)
+            hl7message.ORU_R01_PATIENT_RESULT[i].ORU_R01_ORDER_OBSERVATION.ORU_R01_OBSERVATION.OBX.obx_11 = "F"
+            if critical:
+                hl7message.ORU_R01_PATIENT_RESULT[i].ORU_R01_ORDER_OBSERVATION.ORU_R01_OBSERVATION.OBX.obx_8 = "AA"
+            else:
+                hl7message.ORU_R01_PATIENT_RESULT[i].ORU_R01_ORDER_OBSERVATION.ORU_R01_OBSERVATION.OBX.obx_6 = "N"
+        print_hl7_message(hl7message)
+
+    print hl7message.validate()
     return get_hl7_string(hl7message)
 
